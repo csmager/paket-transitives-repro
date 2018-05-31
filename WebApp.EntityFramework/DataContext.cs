876696de01488130d19348a988ba1ed92a4cfe91
Charles Mager <charlie@csmager.co.uk>
2018-05-31 10:48:25 +0100

    Initial commit

---
+++ WebApp.EntityFramework/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<Item> Items { get; set; }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+    }
+}
